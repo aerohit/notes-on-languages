b9a67c0deaa4277c0d749b989ed9df847dab3955
Rohit Saxena <aerohit.saxena@gmail.com>
2015-02-25 23:29:26 +0100

    Added Tree DS

--- haskell/lyah/making-types-and-typeclasses.hs
+++ haskell/lyah/making-types-and-typeclasses.hs
@@ -155,3 +155,16 @@ Empty' .++ ys     = ys
 (x :-: xs) .++ ys = x :-: (xs .++ ys)
 
 bbList = bList .++ bList
+
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node y l r)
+    | x == y = Node y l r
+    | x < y  = Node y (treeInsert x l) r
+    | x > y  = Node y l (treeInsert x r)
