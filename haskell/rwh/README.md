76c8b2957f7a0679ca6610f9944da16e7e396c10
Rohit Saxena <aerohit.saxena@gmail.com>
2015-05-03 17:30:22 -0400

    Deleted code from RWH

--- haskell/rwh/README.md
+++
@@ -1,3 +0,0 @@
-### Notes
-
-* Skipped chapter 1
--- haskell/rwh/c2-types-and-functions.hs
+++
@@ -1,5 +0,0 @@
--- Function application
-
-myDrop n xs = if n <= 0 || null xs
-                then xs
-                else myDrop (n - 1) (tail xs)
